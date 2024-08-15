c8f20c4ee420e73befac4a1418aedafdb63db25a
suhasarali <suhasarali22@gmail.com>
2024-08-15 21:57:39 +0530

    Add greet method

--- Main.java
+++ Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println(Hello, World!);
+        greet();
+    }
+
+    public static void greet() {
+        System.out.println(Greetings from Git!);
     }
 }
